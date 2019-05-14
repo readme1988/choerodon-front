e0a5090bae7b20cfbc0c72a96f9c0a2d9999c75c
Fan <fan@choerodon.io>
2019-05-14 16:29:39 +0800

    [IMP] update config

--- react/config.js
+++ react/config.js
@@ -3,14 +3,13 @@ const config = {
     clientId: 'localhost', // 必须填入响应的客户端（本地开发）
     titlename: 'Choerodon | 企业数字化服务平台', //项目页面的title名称
     favicon: 'favicon.ico', //项目页面的icon图片名称
-    fileServer: 'http://minio.choerodon.example.com',
     theme: {
       'primary-color': '#3F51B5',
     },
     cookieServer: '', //子域名token共享
-    server: 'http://api.c7nf.choerodon.staging.saas.hand-china.com',
-    fileServer: 'http://minio.staging.saas.hand-china.com',
-    webSocketServer: 'ws://notify.staging.saas.hand-china.com',
+    server: 'http://api.choerodon.example.com',
+    fileServer: 'http://minio.choerodon.example.com',
+    webSocketServer: 'ws://notify.choerodon.example.com',
     buildType: 'single',
     projectType: 'choerodon',
     master: '@choerodon/master',
@@ -26,9 +25,9 @@ const config = {
         "@choerodon/devops",
         "@choerodon/apim"
     ],
-  resourcesLevel: ['site', 'organization', 'project', 'user'],
-  dashboard: {},
-  guide:{}
+    resourcesLevel: ['site', 'organization', 'project', 'user'],
+    dashboard: {},
+    guide:{}
 };
   
 module.exports = config;
