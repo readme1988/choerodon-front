0c50b77d19a93d56d51658dc3413bc7837836b0e
Fan <fan@choerodon.io>
2019-05-14 17:18:54 +0800

    [IMP] update config

--- react/config.js
+++ react/config.js
@@ -1,11 +1,5 @@
 const config = {
-    local: true, //是否为本地开发
     clientId: 'localhost', // 必须填入响应的客户端（本地开发）
-    titlename: 'Choerodon | 企业数字化服务平台', //项目页面的title名称
-    favicon: 'favicon.ico', //项目页面的icon图片名称
-    theme: {
-      'primary-color': '#3F51B5',
-    },
     cookieServer: '', //子域名token共享
     server: 'http://api.choerodon.example.com',
     fileServer: 'http://minio.choerodon.example.com',
