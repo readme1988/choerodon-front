6ae07604da910ab822a2002a3ba699b071c48c00
Fan <fan@choerodon.io>
2019-05-17 10:52:55 +0800

    [IMP] update pacakge.json

--- react/config.js
+++ react/config.js
@@ -6,9 +6,12 @@ const config = {
     cookieServer: '', 
     clientId: 'localhost', // 必须填入响应的客户端（本地开发）
     cookieServer: '', //子域名token共享
-    server: 'http://api.choerodon.example.com',
-    fileServer: 'http://minio.choerodon.example.com',
-    webSocketServer: 'ws://notify.choerodon.example.com',
+  //   server: 'http://api.choerodon.example.com',
+  //   fileServer: 'http://minio.choerodon.example.com',
+  // webSocketServer: 'ws://notify.choerodon.example.com',
+    server: 'http://api.staging.saas.hand-china.com',
+    fileServer: 'http://minio.staging.saas.hand-china.com',
+    webSocketServer: 'ws://notify.staging.saas.hand-china.com',
     buildType: 'single',
     projectType: 'choerodon',
     master: '@choerodon/master',
@@ -17,16 +20,33 @@ const config = {
         '@choerodon/asgard',
         '@choerodon/notify',
         '@choerodon/manager',
-        "@choerodon/agile",
-        "@choerodon/issue",
-        "@choerodon/testManager",
-        "@choerodon/wiki",
-        "@choerodon/devops",
-        "@choerodon/apim"
+        // "@choerodon/agile",
+        // "@choerodon/issue",
+        // "@choerodon/testManager",
+        // "@choerodon/wiki",
+        // "@choerodon/devops",
+        // "@choerodon/apim"
     ],
     resourcesLevel: ['site', 'organization', 'project', 'user'],
-    dashboard: {},
-    guide:{}
+    dashboard: {
+      iam: {
+        components: './node_modules/@choerodon/iam/lib/src/app/iam/dashboard/*',
+        locale: './node_modules/@choerodon/iam/lib/src/app/iam/locale/dashboard/*',
+      },
+      // devops: {
+      //   components: './node_modules/@choerodon/devops/lib/src/app/devops/dashboard/*',
+      //   locale: './node_modules/@choerodon/devops/lib/src/app/devops/locale/dashboard/*',
+      // },
+      // agile: './node_modules/@choerodon/agile/lib/src/app/agile/dashboard/*',
+      // wiki: './node_modules/@choerodon/wiki/lib/src/app/wiki/dashboard/*',
+      // testManager: './node_modules/@choerodon/testManager/lib/src/app/testManager/dashboard/*',
+    },
+  guide: {
+    iam: {
+      components: './node_modules/@choerodon/iam/lib/src/app/iam/guide/*',
+      locale: './node_modules/@choerodon/iam/lib/src/app/iam/locale/guide/*',
+    },
+    }
 };
   
 module.exports = config;
