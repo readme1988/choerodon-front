1426a1e5e2b6c889f6ebdaa0d64f4bd133ea9f40
Fan <fan@choerodon.io>
2019-06-19 16:23:33 +0800

    [IMP] update package.json

--- react/config.js
+++ react/config.js
@@ -38,10 +38,6 @@ const config = {
       components: './node_modules/@choerodon/iam/lib/src/app/iam/dashboard/*',
       locale: './node_modules/@choerodon/iam/lib/src/app/iam/locale/dashboard/*',
     },
-    devops: {
-      components: './node_modules/@choerodon/devops/lib/src/app/devops/dashboard/*',
-      locale: './node_modules/@choerodon/devops/lib/src/app/devops/locale/dashboard/*',
-    },
     agile: './node_modules/@choerodon/agile/lib/src/app/agile/dashboard/*',
     wiki: './node_modules/@choerodon/wiki/lib/src/app/wiki/dashboard/*',
     testManager: './node_modules/@choerodon/testManager/lib/src/app/testManager/dashboard/*',
